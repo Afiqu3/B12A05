aaa863998f9fa672d83620e3fa9cbd5aaac9c2a3
Afiqu3 <gahossainj@gmail.com>
2025-08-28 22:05:34 +0600

    Final code

--- js/script.js
+++ js/script.js
@@ -42,21 +42,55 @@ for (let i = 1; i <= numOfCopyButtons; i++) {
 
 // call button functionality
 function callService(x) {
-  document.getElementById("call-btn" + x).addEventListener("click", function () {
-    const service = document.getElementById("service" + x).innerText;
-    const number = document.getElementById("textToCopy" + x).innerText;
-    let availableCoins = parseInt(document.getElementById("coin-cnt").innerText);
-    if (availableCoins < 20) {
-      alert("Insufficient coins to make a call.");
-      return;
-    }
-    alert("📞 Calling " + service + " at " + number);
-    availableCoins -= 20;
-    document.getElementById("coin-cnt").innerText = availableCoins;
-    callHistory.push({ service: service, number: number , time: new Date().toLocaleTimeString()});
-  });
+  document
+    .getElementById("call-btn" + x)
+    .addEventListener("click", function () {
+      // console.log("Call button for service " + x);
+      const service = document.getElementById("service" + x).innerText;
+      const number = document.getElementById("textToCopy" + x).innerText;
+      let availableCoins = parseInt(
+        document.getElementById("coin-cnt").innerText
+      );
+      if (availableCoins < 20) {
+        alert("Insufficient coins to make a call.");
+      } else {
+        alert("📞 Calling " + service + " at " + number);
+        availableCoins -= 20;
+        document.getElementById("coin-cnt").innerText = availableCoins;
+        callHistory.push({
+          service: service,
+          number: number,
+          time: new Date().toLocaleTimeString(),
+        });
+      }
+      // call history functionality
+      const historyContainer = document.getElementById("history-list");
+      historyContainer.innerHTML = "";
+      for (const history of callHistory) {
+        const historyEntry = document.createElement("div");
+        historyEntry.className = "card mb-2";
+        historyEntry.innerHTML = `
+    <div class="flex justify-between items-center bg-[#fafafa] p-3">
+      <div>
+        <h5 class="font-bold">${history.service}</h5>
+        <p class="text-sm text-[#5c5c5c]">${history.number}</p>
+      </div>
+      <div>
+          <p>${history.time}</p>
+      </div>
+    </div>
+  `;
+        historyContainer.appendChild(historyEntry);
+      }
+    });
 }
 for (let i = 1; i <= numOfCallButtons; i++) {
   callService(i);
-  console.log(callHistory);
 }
+
+// history clear functionality
+document.getElementById("clean-history").addEventListener("click", function () {
+  // console.log("Clearing call history");
+  callHistory = [];
+  document.getElementById("history-list").innerHTML = "";
+});
